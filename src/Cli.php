f40135c0914e53afbf06cc1389c6c67875eb175a
Maria <baraymasha@gmail.com>
2022-02-28 10:09:13 +0300

    Modified Cli.php

--- src/Cli.php
+++ src/Cli.php
@@ -8,7 +8,7 @@ use function cli\prompt;
 function welcome()
 {
     line('Welcome to the Brain Games!');
-    $name = prompt('May I have your name?', '', ' ', false);
+    $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
     return $name;
 }
