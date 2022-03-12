8585948f83c18339e07d338e388ac47b2b0eb57e
Maria <baraymasha@gmail.com>
2022-03-12 21:39:19 +0300

    Delete whitespace

--- src/Engine.php
+++ src/Engine.php
@@ -6,7 +6,7 @@ use function cli\line;
 use function cli\prompt;
 
 function checkGame(string $name, string $gameName)
-{    
+{
     $functionTask = "\\Brain\\Games\\{$gameName}\\task";
     if (function_exists($functionTask)) {
         line($functionTask());
